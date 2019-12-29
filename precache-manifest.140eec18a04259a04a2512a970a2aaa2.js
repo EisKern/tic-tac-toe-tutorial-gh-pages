001798f136d1025eb4cbcf9648500d5443067885
EisKern <EisKern@users.noreply.github.com>
2019-12-30 01:05:25 +0900

    Updates

---
+++ precache-manifest.140eec18a04259a04a2512a970a2aaa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61af776601d1d6aeec2346e661a36bc",
+    "url": "/tic-tac-toe-tutorial-gh-pages/index.html"
+  },
+  {
+    "revision": "0580dea4a025a217bbe8",
+    "url": "/tic-tac-toe-tutorial-gh-pages/static/css/main.8a935685.chunk.css"
+  },
+  {
+    "revision": "bcb866a3cf9e0efa1d30",
+    "url": "/tic-tac-toe-tutorial-gh-pages/static/js/2.6a465692.chunk.js"
+  },
+  {
+    "revision": "0580dea4a025a217bbe8",
+    "url": "/tic-tac-toe-tutorial-gh-pages/static/js/main.549fb610.chunk.js"
+  },
+  {
+    "revision": "7c59f0083c0ad5a024cf",
+    "url": "/tic-tac-toe-tutorial-gh-pages/static/js/runtime~main.0e043dc2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-toe-tutorial/precache-manifest.155cadb099ce2c5adfc274eb3177ce13.js"
+  "/tic-tac-toe-tutorial-gh-pages/precache-manifest.140eec18a04259a04a2512a970a2aaa2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe-tutorial/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe-tutorial-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
